1a89b8429c8b3a85b5d067ec62ebcf1669a645f9
NuthX <huermos@gmail.com>
2025-04-28 15:12:21 +0800

    feat(check): 每分钟检查psn在线状态

--- instrumentation.js
+++ instrumentation.js
@@ -1,6 +1,8 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { initUser } = await import("@/lib/prisma")
+    const { startTask } = await import("@/lib/schedule")
     await initUser();
+    await startTask();
   }
 }
