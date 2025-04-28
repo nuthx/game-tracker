9f2676ff2fe47b22e65416641e97026c16ae9084
NuthX <huermos@gmail.com>
2025-04-28 17:05:04 +0800

    feat(core): 实现游戏时间记录功能

--- instrumentation.js
+++ instrumentation.js
@@ -2,7 +2,9 @@ export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { initUser } = await import("@/lib/prisma")
     const { startTask } = await import("@/lib/schedule")
+    const { closeErrorRecord } = await import("@/lib/check")
     await initUser();
     await startTask();
+    await closeErrorRecord();
   }
 }
