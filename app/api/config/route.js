6c6b83e40e6289afb4931b4124bf395f67cebd5b
NuthX <huermos@gmail.com>
2025-04-29 16:03:54 +0800

    feat(settings): 搭建设置页面

--- app/api/config/route.js
+++ app/api/config/route.js
@@ -1,5 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { sendResponse } from "@/lib/response";
+import { sendResponse } from "@/lib/http";
 
 export async function GET(request) {
   try {
@@ -7,8 +7,10 @@ export async function GET(request) {
     return sendResponse(request, {
       data: { 
         username: user.username,
+        avatar: user.avatar,
+        psnId: user.psnId,
         npsso: `${user.npsso.slice(0, 4)}****${user.npsso.slice(-4)}`,
-        psnId: user.psnId
+        monitorId: user.monitorId
       }
     });
   } catch (error) {
@@ -23,6 +25,20 @@ export async function PATCH(request) {
   try {
     const data = await request.json();
 
+    if (data.new_npsso) {
+      await prisma.user.update({
+        where: { id: 1 },
+        data: { npsso: data.new_npsso }
+      });
+    }
+
+    if (data.new_monitorId) {
+      await prisma.user.update({
+        where: { id: 1 },
+        data: { monitorId: data.new_monitorId }
+      });
+    }
+
     if (data.new_username) {
       await prisma.user.update({
         where: { id: 1 },
@@ -34,20 +50,6 @@ export async function PATCH(request) {
       await prisma.user.update({
         where: { id: 1 },
         data: { password: data.new_password }
-      });
-    }
-
-    if (data.new_npsso) {
-      await prisma.user.update({
-        where: { id: 1 },
-        data: { npsso: data.new_npsso }
-      });
-    }
-
-    if (data.new_psnId) {
-      await prisma.user.update({
-        where: { id: 1 },
-        data: { psnId: data.new_psnId }
       });
     }
 
---
+++ app/settings/page.js
@@ -0,0 +1,152 @@
+"use client";
+
+import Image from "next/image"
+import { useEffect } from "react";
+import { useData, API } from "@/lib/swr";
+import { handleRequest } from "@/lib/http";
+import { createForm } from "@/lib/form";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
+export default function Page() {
+  const { data: configData, error: configError, isLoading: configLoading, mutate: configMutate } = useData(API.CONFIG);
+
+  const npssoForm = createForm({
+    new_npsso: { schema: "required" },
+  })();
+
+  const monitorForm = createForm({
+    new_monitorId: { schema: "required" },
+  })();
+
+  const userForm = createForm({
+    new_username: { schema: "username" },
+    new_password: { schema: "password" }
+  })();
+
+  useEffect(() => {
+    if (configData?.username) {
+      monitorForm.setValue("new_monitorId", configData.monitorId);
+      userForm.setValue("new_username", configData.username);
+    }
+  }, [configData]);
+
+  const handleConfig = async (values) => {
+    const result = await handleRequest("PATCH", API.CONFIG, values);
+    if (result) {
+      configMutate();
+    }
+  };
+
+  if (configLoading) {
+    return <div className="flex justify-center text-sm text-muted-foreground">加载中...</div>;
+  }
+
+  if (configError) {
+    return <div className="flex justify-center text-sm text-muted-foreground">配置信息获取失败</div>;
+  }
+
+  return (
+    <div className="flex gap-6">
+      <div className="flex flex-col gap-6 flex-1">
+        <Card>
+          <CardHeader className="gap-0">
+            <CardTitle>PSN账号</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...npssoForm}>
+              <form onSubmit={npssoForm.handleSubmit((values) => handleConfig(values))} className="space-y-6" noValidate>
+                <FormField control={npssoForm.control} name="new_npsso" render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>NPSSO</FormLabel>
+                    <FormControl>
+                      <Input className="w-full" placeholder={configData.npsso === "****" ? "" : configData.npsso} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )} />
+                <Button type="submit">保存</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="gap-0">
+            <CardTitle>监控设置</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...monitorForm}>
+              <form onSubmit={monitorForm.handleSubmit((values) => handleConfig(values))} className="space-y-6" noValidate>
+                <FormField control={monitorForm.control} name="new_monitorId" render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>PSNID</FormLabel>
+                    <FormControl>
+                      <Input className="w-full" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )} />
+                <Button type="submit">保存</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="gap-0">
+            <CardTitle>用户资料</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Form {...userForm}>
+              <form onSubmit={userForm.handleSubmit((values) => handleConfig(values))} className="space-y-6" noValidate>
+                <FormField control={userForm.control} name="new_username" render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>用户名</FormLabel>
+                    <FormControl>
+                      <Input className="w-full" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )} />
+                <FormField control={userForm.control} name="new_password" render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>新密码</FormLabel>
+                    <FormControl>
+                      <Input className="w-full" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )} />
+                <Button type="submit">保存</Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+
+      <Card className="w-80 h-fit">
+        <CardContent className="flex flex-col items-center gap-4">
+          <Image src="https://blog.playstation.com/tachyon/2028/04/cb6472ba78b0978c5190f3abd6897c1e1ce1eb01.png" alt="User Avatar" className="rounded-full object-cover w-30 h-30" width={120} height={120} priority draggable="false"/>
+          <div className="flex flex-col items-center gap-1">
+            <p className="text-lg font-bold">Name</p>
+            <p className="text-sm text-muted-foreground">PSNID: 123456789</p>
+          </div>
+          <Button variant="secondary" className="mt-2 w-full shadow-none">登出系统</Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
