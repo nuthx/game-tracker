d7f3275c97a198359a2c4a26c2dd1fd33a6e20b7
NuthX <huermos@gmail.com>
2025-04-30 12:18:52 +0800

    feat(home): 首页游玩中游戏卡片

---
+++ app/api/presence/route.js
@@ -0,0 +1,36 @@
+import { getBasicPresence } from "psn-api";
+import { prisma } from "@/lib/prisma";
+import { getAuthorization } from "@/lib/auth";
+import { sendResponse } from "@/lib/http";
+
+export async function GET(request) {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: 1 } });
+    const authorization = await getAuthorization();
+    const presence = await getBasicPresence(authorization, user.monitorId);
+
+    // 计算游戏时长
+    let gameTime = 0;
+    if (presence.basicPresence.gameTitleInfoList?.length) {
+      const lastGame = await prisma.psnRecord.findFirst({
+        where: {
+          endAt: null,
+          user: { some: { id: user.id } }
+        }
+      });
+      gameTime = Math.floor((new Date() - new Date(lastGame.startAt)) / 1000 / 60);
+    }
+
+    return sendResponse(request, { 
+      data: {
+        ...presence.basicPresence,
+        gameTime
+      }
+    });
+  } catch (error) {
+    return sendResponse(request, {
+      code: 500,
+      message: error.message,
+    });
+  }
+}
--- app/page.js
+++ app/page.js
@@ -1,25 +1,37 @@
 "use client";
 
+import Image from "next/image";
 import { useData, API } from "@/lib/swr";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
 
 export default function Page() {
-  const { data, error, isLoading } = useData(API.USERNAME);
-  const { data: password, error: passwordError, isLoading: passwordLoading } = useData(API.PASSWORD);
-  const { data: npsso, error: npssoError, isLoading: npssoLoading } = useData(API.NPSSO);
+  const { data: presenceData, error: presenceError, isLoading: presenceLoading } = useData(API.PRESENCE);
 
-  if (isLoading) {
-    return <div>加载中...</div>;
+  if (presenceLoading) {
+    return <div className="flex justify-center text-sm text-muted-foreground">加载中...</div>;
   }
 
-  if (error) {
-    return <div>获取用户名失败</div>;
+  if (presenceError) {
+    return <div className="flex justify-center text-sm text-muted-foreground">用户信息获取失败</div>;
   }
 
   return (
-    <div>
-      当前用户：{data?.username}
-      当前密码：{password?.password}
-      当前NPSSO：{npsso?.npsso}
+    <div className="flex flex-row gap-4 md:gap-6">
+      {presenceData.gameTitleInfoList?.length && (
+        <Card className="w-full">
+          <CardContent className="flex flex-row items-center gap-5">
+            <Image src={presenceData.gameTitleInfoList[0]?.conceptIconUrl || presenceData.gameTitleInfoList[0]?.npTitleIconUrl} alt={presenceData.gameTitleInfoList[0]?.titleName} className="rounded-md object-cover w-24 h-24" width={96} height={96} priority draggable="false"/>
+            <div className="flex flex-col gap-1.5">
+              <p className="text-sm text-muted-foreground">正在游玩 {presenceData.gameTitleInfoList[0].launchPlatform.toUpperCase()} 游戏</p>
+              <p className="font-bold">{presenceData.gameTitleInfoList[0].titleName}</p>
+              <p className="text-sm text-muted-foreground">已游玩 {presenceData.gameTime} 分钟</p>
+            </div>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
