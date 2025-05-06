f9bf93734545e0758550df6264ad65be987dc2b2
NuthX <huermos@gmail.com>
2025-05-06 14:08:31 +0800

    feat(i18n): 多语言支持

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import "@/app/globals.css";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { I18nWrapper } from "@/i18n/wrapper";
 import { Toaster } from "@/components/ui/sonner"
 import { NavBar } from "@/components/navbar";
 
@@ -12,11 +13,13 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="bg-accent/50 overflow-y-scroll">
         <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          <NavBar />
-          <div className="container mx-auto max-w-screen-lg flex flex-col gap-4 p-4 md:p-8">
-            {children}
-          </div>
-          <Toaster position="top-center" />
+          <I18nWrapper>
+            <NavBar />
+            <div className="container mx-auto max-w-screen-lg flex flex-col gap-4 p-4 md:p-8">
+              {children}
+            </div>
+            <Toaster position="top-center" />
+          </I18nWrapper>
         </NextThemesProvider>
       </body>
     </html>
