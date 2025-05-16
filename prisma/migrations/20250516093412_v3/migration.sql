82566c384b62fc2c64bc26afa4c59befed9fe64b
NuthX <huermos@gmail.com>
2025-05-16 17:43:06 +0800

    feat(record): psn记录用户id和名字

--- prisma/migrations/20250516093412_v3/migration.sql
+++ prisma/migrations/20250516093412_v3/migration.sql
@@ -1,5 +1,7 @@
 /*
   1. 添加NxRecord表
+  2. 新增PsnRecord字段userId，如果monitorId为me则设置为accountId，否则设置为monitorId
+  3. 新增NxRecord字段userName，默认值为空
 */
 
 -- CreateTable
@@ -20,6 +22,39 @@ CREATE TABLE "NxRecord" (
 -- RedefineTables
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_PsnRecord" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "state" TEXT NOT NULL,
+    "npTitleId" TEXT NOT NULL DEFAULT '',
+    "titleName" TEXT NOT NULL DEFAULT '',
+    "format" TEXT NOT NULL DEFAULT '',
+    "launchPlatform" TEXT NOT NULL DEFAULT '',
+    "conceptIconUrl" TEXT NOT NULL DEFAULT '',
+    "userId" TEXT NOT NULL DEFAULT '',
+    "userName" TEXT NOT NULL DEFAULT '',
+    "startAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "endAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "playSeconds" INTEGER NOT NULL DEFAULT 0
+);
+INSERT INTO "new_PsnRecord" ("conceptIconUrl", "endAt", "format", "id", "launchPlatform", "npTitleId", "playSeconds", "startAt", "state", "titleName", "userId")
+SELECT 
+    p."conceptIconUrl", 
+    p."endAt", 
+    p."format", 
+    p."id", 
+    p."launchPlatform", 
+    p."npTitleId", 
+    p."playSeconds", 
+    p."startAt", 
+    p."state", 
+    p."titleName",
+    CASE 
+        WHEN (SELECT "monitorId" FROM "User" WHERE "id" = 1) = 'me' THEN (SELECT "accountId" FROM "User" WHERE "id" = 1)
+        ELSE (SELECT "monitorId" FROM "User" WHERE "id" = 1)
+    END
+FROM "PsnRecord" p;
+DROP TABLE "PsnRecord";
+ALTER TABLE "new_PsnRecord" RENAME TO "PsnRecord";
 CREATE TABLE "new_User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "username" TEXT NOT NULL,
