8c27a1fbb65c8a6de22a588f560b8a3c3415e1b5
NuthX <huermos@gmail.com>
2025-05-23 21:48:23 +0800

    feat(settings): 弹窗触发器放在nav中

--- components/settings.jsx
+++ components/settings.jsx
@@ -1,17 +1,10 @@
 import { useState } from "react"
 import { useTranslation } from "react-i18next"
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle
-} from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
-import { Settings2, Gamepad2, CircleUser, Database } from "lucide-react"
+import { Gamepad2, CircleUser, Database } from "lucide-react"
 
 export function Settings() {
   const { t } = useTranslation()
-  const [open, setOpen] = useState(false)
 
   const items = [
     {
@@ -45,57 +38,48 @@ export function Settings() {
   const CurrentComponent = items.find((item) => item.title === activeComponent)?.component
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button variant="ghost" className="size-10">
-          <Settings2 className="size-5" />
-        </Button>
-      </DialogTrigger>
-
-      <DialogContent className="flex gap-0 p-0 h-[90vh] max-h-[720px] w-[80vw] sm:max-w-[1160px] overflow-hidden">
-        <DialogTitle className="sr-only">{t("settings.title")}</DialogTitle>
-        <div className="flex flex-col gap-6 p-3 w-60 bg-accent/70 border-r shrink-0">
-          <div className="flex flex-col gap-1">
-            <p className="text-sm font-medium text-muted-foreground mx-[10px] my-1">{t("settings.menu.monitor")}</p>
-            {items.slice(0, 3).map((item) => (
-              <Button
-                key={item.title}
-                variant="ghost"
-                size="sm"
-                className={`w-full justify-start font-normal hover:bg-neutral-200/70 ${
-                  activeComponent === item.title ? "bg-neutral-200/70" : ""}
-                `}
-                onClick={() => setActiveComponent(item.title)}
-              >
-                <item.icon className="size-4 mr-1" />
-                {item.title}
-              </Button>
-            ))}
-          </div>
-          <div className="flex flex-col gap-1">
-            <p className="text-sm font-medium text-muted-foreground mx-[10px] my-1">{t("settings.menu.system")}</p>
-            {items.slice(3).map((item) => (
-              <Button
-                key={item.title}
-                variant="ghost"
-                size="sm"
-                className={`w-full justify-start font-normal hover:bg-neutral-200/70 ${
-                  activeComponent === item.title ? "bg-neutral-200/70" : ""}
-                `}
-                onClick={() => setActiveComponent(item.title)}
-              >
-                <item.icon className="size-4 mr-1" />
-                {item.title}
-              </Button>
-            ))}
-          </div>
-          <Button size="sm" variant="destructive" className="mt-auto">{t("btn.logout")}</Button>
+    <>
+      <div className="flex flex-col gap-6 p-3 w-60 bg-accent/70 border-r shrink-0">
+        <div className="flex flex-col gap-1">
+          <p className="text-sm font-medium text-muted-foreground mx-[10px] my-1">{t("settings.menu.monitor")}</p>
+          {items.slice(0, 3).map((item) => (
+            <Button
+              key={item.title}
+              variant="ghost"
+              size="sm"
+              className={`w-full justify-start font-normal hover:bg-neutral-200/70 ${
+                activeComponent === item.title ? "bg-neutral-200/70" : ""}
+              `}
+              onClick={() => setActiveComponent(item.title)}
+            >
+              <item.icon className="size-4 mr-1" />
+              {item.title}
+            </Button>
+          ))}
         </div>
-        <div className="h-full p-8">
-          <CurrentComponent />
+        <div className="flex flex-col gap-1">
+          <p className="text-sm font-medium text-muted-foreground mx-[10px] my-1">{t("settings.menu.system")}</p>
+          {items.slice(3).map((item) => (
+            <Button
+              key={item.title}
+              variant="ghost"
+              size="sm"
+              className={`w-full justify-start font-normal hover:bg-neutral-200/70 ${
+                activeComponent === item.title ? "bg-neutral-200/70" : ""}
+              `}
+              onClick={() => setActiveComponent(item.title)}
+            >
+              <item.icon className="size-4 mr-1" />
+              {item.title}
+            </Button>
+          ))}
         </div>
-      </DialogContent>
-    </Dialog>
+        <Button size="sm" variant="destructive" className="mt-auto">{t("btn.logout")}</Button>
+      </div>
+      <div className="h-full p-8">
+        <CurrentComponent />
+      </div>
+    </>
   )
 }
 
