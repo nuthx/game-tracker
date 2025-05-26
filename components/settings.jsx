853488b08656cd0a1d1ed88988535fa49a7d8720
NuthX <huermos@gmail.com>
2025-05-26 16:45:37 +0800

    fix(nximport): 修复函数名错误

--- components/settings.jsx
+++ components/settings.jsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Separator } from "@/components/ui/separator"
 import { Avatar } from "@/components/avatar"
-import { ImportRecord, ExportRecord, DeleteRecord, ImportNxRecordDrag } from "@/components/record"
+import { ImportRecord, ExportRecord, DeleteRecord, ImportNxRecord } from "@/components/record"
 import { Gamepad2, CircleUser, Database, ArrowRight } from "lucide-react"
 
 export function Settings() {
