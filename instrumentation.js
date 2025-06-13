bafab84475c5a2e302a59d823ba4fc8e3a457b03
NuthX <huermos@gmail.com>
2025-06-13 09:48:42 +0800

    perf(platform): 不再初始化默认平台

--- instrumentation.js
+++ instrumentation.js
@@ -1,10 +1,8 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
     const { initConfig } = await import("@/lib/prisma")
-    const { initPlatform } = await import("@/lib/prisma")
     const { startTask } = await import("@/lib/schedule")
     await initConfig()
-    await initPlatform()
     await startTask()
   }
 }
